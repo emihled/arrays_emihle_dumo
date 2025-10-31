37c33905f24325ed9d8f514aef2b5a8cb985893e
emihled <emihledumo@gmail.com>
2025-10-31 10:20:35 +0200

    arrays in javascpt

---
+++ array.js
@@ -0,0 +1,13 @@
+color = [];
+color.push("Black", "Velvet", "Blue", "White", "Red");
+console.log(color);
+number = [1,2,3,4,5,6,7,8,9];
+console.log(number);
+changecolor = color.pop();
+console.log(color);
+addcolor = color.unshift("Brown");
+console.log(color);
+
+console.log(color.length);
+console.log(number.length);
+
